27b4d229d120a85dbcfb5e211611251b0a9f1091
Aakash Singh <aakash.singh.0091@gmail.com>
2015-06-27 10:15:40 -0400

    First commit with the required changes

---
+++ AugRealTakeTwo/app/src/main/java/com/example/aakash/augrealtaketwo/performUpdate.java
@@ -0,0 +1,32 @@
+package com.example.aakash.augrealtaketwo;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Aakash on 6/22/2015.
+ */
+public class performUpdate extends AsyncTask<Integer, Integer, String[]>
+{
+
+    @Override
+    protected String[] doInBackground(Integer... params) {
+        String[] updateView = new String[10];
+        int i = params[0];
+        // Where all the info fetched from webservice would come in
+        updateView[0]=Integer.toString(params[0]);
+        updateView[1]="Sunny";
+        updateView[2]="Going to get hotter soon";
+        updateView[3]="Kanata";
+        updateView[4]="31st May 2015";
+        return updateView;
+    }
+
+    @Override
+    protected void onPostExecute(String[] strings) {
+        MainActivity.temperature.setText(strings[0]);
+        MainActivity.weather_icon.setText(strings[1]);
+        MainActivity.city_field.setText(strings[3]);
+        MainActivity.updated_field.setText(strings[4]);
+        MainActivity.temperaturePic.setImageResource(R.drawable.sunnywithclouds);
+    }
+}
